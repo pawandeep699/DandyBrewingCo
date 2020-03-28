adc1468d66cc75839ab5be02f936a89e87806d30
Pawandeep Singh Grewal <59664711+pawandeep699@users.noreply.github.com>
2020-03-28 10:26:48 -0600

    Add files via upload

---
+++ Dandy Brewaing co . Peace Group/ageRestriction.js
@@ -0,0 +1,44 @@
+const age = document.querySelector("#age");
+
+const button = document.querySelector("#submit")
+const show = document.querySelector("#display");
+const clean = document.querySelector("#erase")
+function call(num){
+
+    num = age.value
+    if(num>=18){
+    var ut = "Click here to enter ";
+    var open = ut.link("mainPage.html");
+
+    document.getElementById("display").innerHTML =  open;
+
+ 
+        
+    }
+
+    else if (num<18 && num>0){
+
+       return document.getElementById("display").textContent =  "You are not Eligible ";
+    }
+
+    else{
+
+        return document.getElementById("display").textContent = "Invalid Input";
+    }
+}
+
+function  submit(){
+
+    show.value = call();
+
+}
+
+function erase(){
+
+    document.getElementById("display").textContent = null ;
+  
+}
+
+
+button.addEventListener("click",submit);
+clean.addEventListener("click",erase);
